2fec9ae1a5b5a4c2398064aff547feffdc9a828a
Kate <vvellspring@gmail.com>
2022-09-13 15:01:48 +0300

    Двумерный массив: заменить четные позиции на их квадраты

---
+++ Seminar7_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых обе позиции четные,
+// и замените их элементы на их квадраты.
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[,] FillArray(int numLine, int NumColumns)
+{
+    int[,] matrix = new int[numLine, NumColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)     // Строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // Столбец
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ConvertToSqrt(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] myArray = FillArray(5, 5);
+PrintArray(myArray);
+Console.WriteLine();
+PrintArray(ConvertToSqrt(myArray));
+
+
